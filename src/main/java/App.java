328ec255047304f20d8d8b7d17af6c6ea4c9eb83
Andreas Munte <andreas.munte@exxcellent.de>
2021-09-30 21:24:23 +0200

    Adding dummy main

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+/**
+ * Dummy Main class
+ *
+ * @author Maglethong spirr
+ * @since 30/09/2021
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Goodbye!");
+    }
+}
